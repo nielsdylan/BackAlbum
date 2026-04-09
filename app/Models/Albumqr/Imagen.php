92ac0a12f16e49078b424ac711fb636228b4da88
nielsdylan <niels_dylan@hotmail.com>
2026-04-08 22:52:30 -0500

    modelo

--- app/Models/Albumqr/Imagen.php
+++ app/Models/Albumqr/Imagen.php
@@ -10,7 +10,7 @@ class Imagen extends Model
 {
     //
     use HasFactory, SoftDeletes;
-    protected $table = 'images';
+    protected $table = 'albumqr.imagenes';
     protected $fillable = ['nombre','name_image','weight', 'path', 'extension', 'user_id'];
     protected $hidden = ['created_at', 'updated_at', 'deleted_at'];
 }
